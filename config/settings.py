7ec44e81ac26e32e59a0bf32964e5995b4f97f27
morethanmin <mini4614@gmail.com>
2021-08-10 15:14:31 +0900

    a

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o19&d*t)-)fuqtnfq4!#ab)&@k#q1_y^@d9xls*yhg+ax2xu#6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
